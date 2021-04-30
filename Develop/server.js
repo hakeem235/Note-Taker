a3646108addb62cb140b347e6a428c2daea4e4d0
ahmed hakeem <hakeemkoo@hotmail.com>
2021-04-30 11:19:07 -0400

    add the delect function

--- Develop/server.js
+++ Develop/server.js
@@ -8,7 +8,9 @@ var PORT = 3000;
 
 var data = JSON.parse(fs.readFileSync("./db/db.json", "utf8"))
 
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({
+    extended: true
+}));
 app.use(express.json());
 app.use("/assets", express.static("./public/assets"));
 
@@ -42,8 +44,27 @@ app.post("/api/notes", (req, res) => {
     res.json(data);
 })
 
+//DELETE A SPECIFIC NOTE 
 
+app.delete("/api/notes/:id", (req, res) => {
+    let noteId = req.params.id;
+
+    let newId = 0;
+    console.log(`Deleting note with ${noteId}`);
+    data = data.filter(currentNote => {
+        return currentNote.id != noteId;
+    });
+    for (currentNote of data) {
+        currentNote.id = newId.toString();
+        newId++;
+    }
+    fs.writeFileSync("./db/db.json", JSON.stringify(data));
+    res.json(data);
+})
 
 // Starts the server to begin listening
 
 app.listen(PORT, () => console.log(`App listening on PORT ${PORT}`))
+
+
+fun
